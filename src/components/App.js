ba5f7277128f9bc7f86e5c44763efe3d1784eac5
You Matsumoto <youmts22@gmail.com>
2020-08-04 07:28:14 +0900

    fix: rename

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import TodoList from '../containers/TodoList'
 
 const App = () => (
   <div>
     <AddTodo />
-    <VisibleTodoList />
+    <TodoList />
     <Footer />
   </div>
 );
